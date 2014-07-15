2d2bd72fefcaa00914563764e71bc5ce5f96fd9d
Nico0084 <nico84dev@gmail.com>
2014-07-15 15:05:35 +0200

    fix conversion unicode issue

--- conversion/from_DT_Switch_to_switch.py
+++ conversion/from_DT_Switch_to_switch.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
 def from_DT_Switch_to_switch(x):
-    if x == '0':
-        print "+++ from_DT_Switch_to_switch : {0} to {1}".format(x,  'Off')        
+    if x == 0:      
         return 'Off'
-    if x == '1':
-        print "+++ from_DT_Switch_to_switch : {0} to {1}".format(x, 'On')        
+    if x == 1: 
         return 'On'
--- conversion/from_switch_to_DT_Switch.py
+++ conversion/from_switch_to_DT_Switch.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-
 def from_switch_to_DT_Switch(x):
     if x == 'Off':
-        print "+++ from_switch_to_DT_Switch : {0} to {1}".format(x,  0)        
         return int(0)
     if x == 'On':
-        print "+++ from_switch_to_DT_Switch : {0} to {1}".format(x,  1)
         return int(1)

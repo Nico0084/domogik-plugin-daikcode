27fad7070a1c8191d02f99b5b0154875b275c3c2
Nico0084 <nico84dev@gmail.com>
2014-07-21 14:59:38 +0200

    Work with new device creation process

--- tests/create_device.py
+++ tests/create_device.py
@@ -2,8 +2,6 @@
 #-*- coding: utf-8 -*-
 
 ### configuration ######################################
-REST_URL="http://192.168.1.195:40405"
-HOST="vmdomogik0"
 DEVICE_NAME="Remote_1"
 DEVICE_IR = "IRTrans_ws"
 DEVICE_DATATYPE = "BinTimings"
@@ -12,21 +10,31 @@ DEVICE_DATATYPE = "BinTimings"
 from domogik.tests.common.testdevice import TestDevice
 from domogik.common.utils import get_sanitized_hostname
 
+plugin =  "daikcode"
+
 def create_device():
     ### create the device, and if ok, get its id in device_id
-    print("Creating the device...")
+    client_id  = "plugin-{0}.{1}".format(plugin, get_sanitized_hostname())
+    print("Creating the Remote device...")
     td = TestDevice()
-    print 'host :',  get_sanitized_hostname()
-    td.create_device("plugin-daikcode.{0}".format(get_sanitized_hostname()), "test_daikcode.remotearc", "daikcode.remotearc")
-    print "Device created"
-    td.configure_global_parameters({"device" : DEVICE_NAME, "irdevice": DEVICE_IR, "datatype": DEVICE_DATATYPE})
-    print "Device configured" 
+    params = td.get_params(client_id, "daikcode.remotearc")
+        # fill in the params
+    params["device_type"] = "daikcode.remotearc"
+    params["name"] = "test_daikcode.remotearc"
+    params["reference"] = "ARC Remote"
+    params["description"] = "Connected to {0}".format(DEVICE_IR)
+    for idx, val in enumerate(params['no-xpl']):
+        if params['no-xpl'][idx]['key'] == 'irdevice' :  params['no-xpl'][idx]['value'] = DEVICE_IR
+        if params['no-xpl'][idx]['key'] == 'datatype' :  params['no-xpl'][idx]['value'] = DEVICE_DATATYPE
+
+    for idx, val in enumerate(params['xpl']):
+        params['xpl'][idx]['value'] = DEVICE_NAME
+
+    # go and create
+    td.create_device(params)
+    print "Device Remote {0} configured".format(DEVICE_NAME) 
+    
 
 if __name__ == "__main__":
     create_device()
 
-
-# TODO : recup de la config rest
-# TODO : passer les parametres a la fonction
-# TODO : renommer le fichier pour pouvoir importer la fonction en lib
-# TODO : create generic functions for device creation and global parameters
